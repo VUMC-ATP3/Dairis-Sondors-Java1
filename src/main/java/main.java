39ff311a80abdc352da763e7be51c44395716ab7
Dairis_Sondors <104448717+DairisSondors@users.noreply.github.com>
2022-05-10 21:28:54 +0300

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,87 @@
+public class main {
+    public static void main(String[] args) {
+
+        int dogAge = 3; // Vesels skaitlis
+        String dogName = "Reksis"; //teksts
+        //(mainīgā tips) (mainīgā nosukums) = (vērtība) ;
+        double dogWeight = 15.8; //daļskaitlis
+        float augumsSuns = 60.23F; //daļskaitlis
+        boolean irIzsalcis=true; //true = 1 ; false = 0
+        boolean irSkirnesSuns=false;
+
+
+
+        char dzimums = 'V';
+        char dzimumsDivi = 'S';
+        char vaditajaKategorija = 'B';
+
+
+
+
+
+
+        System.out.println("Mans suns");
+        System.out.println("Vecums");
+        System.out.println(dogAge);
+        System.out.println("Vārds");
+        System.out.println(dogName);
+        System.out.println("Mana suņa svars");
+        System.out.println(dogWeight+" kg");
+        System.out.println("Mans suņa augstums" + " " +augumsSuns + " cm");
+        System.out.println("Vai mans suns ir izsalcis?");
+        System.out.println(irIzsalcis);
+        System.out.println("Vai mans suns ir skirnes suns");
+        System.out.println(irSkirnesSuns);
+
+
+//(mainīgā tips) (mainiga nosaukums) = (vērtība);
+        int gadaApgrozijumsDolaros = 2147483647;
+        byte monthsInYear = 12;
+        short autoPrice = 12312;
+        long netIncome = 2147483699L;
+        System.out.println(gadaApgrozijumsDolaros + "\n" + monthsInYear + "\n" + autoPrice + "\n" + netIncome);
+
+        int summa;
+        int starpiba;
+        int reizinajums;
+        double dalijums;
+        int atlikums;
+        int x = 5;
+        int y = 10;
+
+        double z= 5;
+        double q= 10;
+        double w = 12;
+
+
+
+
+        starpiba = x - y;
+        summa = x + y;
+        reizinajums = x * y;
+        dalijums = z / q;
+        atlikums = 12%5;
+
+        int skaitlis = 6;
+        System.out.println("Ja atbilde ir 0, šis ir pāra skaitlis " + skaitlis%2);
+
+
+
+        System.out.println("Summa " + summa);
+        System.out.println("Starpība " + starpiba);
+        System.out.println("Reizinājums " + reizinajums);
+        System.out.println("Dalījums " + dalijums);
+        System.out.println("Atlikums " + atlikums);
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
